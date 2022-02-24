b914df7903c2609f065719e8c747381be9ec9558
Philipp898 <ph-ullmann@web.de>
2022-02-24 18:41:47 +0100

    Add: Emulators

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { PartyDetailsComponent } from './components/party-details/party-details.
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFireAuthModule} from '@angular/fire/compat/auth';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFirestoreModule, SETTINGS } from '@angular/fire/compat/firestore';
 import { AddPartyComponent } from './components/add-party/add-party.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule,Routes } from '@angular/router';
@@ -20,6 +20,8 @@ import { AboutComponent } from './components/pages/about/about.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { SigninComponent } from './components/auth/signin/signin.component';
 import { AngularFireAuthGuard } from '@angular/fire/compat/auth-guard';
+import { USE_EMULATOR as USE_AUTH_EMULATOR } from '@angular/fire/compat/auth';
+import { USE_EMULATOR as USE_FIRESTORE_EMULATOR } from '@angular/fire/compat/firestore';
 
 const appRoutes:Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -52,7 +54,17 @@ const appRoutes:Routes = [
     FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    { provide: USE_AUTH_EMULATOR, useValue: environment.useEmulators ? ['http://localhost:9099'] : undefined },
+    { provide: USE_FIRESTORE_EMULATOR, useValue: environment.useEmulators ? ['http://localhost:8080'] : undefined },
+    {
+      provide: SETTINGS,
+      useValue: environment.useEmulators ?  {
+        host: 'localhost:8080',
+        ssl: false
+      }: undefined 
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
